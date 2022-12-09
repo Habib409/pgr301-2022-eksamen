e5682329f736f372b3cf8e7a47492ab4fda825a1
Habib409 <hahi003@student.kristiania.no>
2022-12-09 01:43:00 +0100

    added extra widgets v6

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -39,7 +39,43 @@ resource "aws_cloudwatch_dashboard" "main" {
         "period": 300,
         "stat": "Maximum",
         "region": "eu-west-1",
-        "title": "Total carts"
+        "title": "Total carts currently"
+      }
+    }    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.candidate_id}",
+            "account_count.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Total check outs"
+      }
+    },    {
+      "type": "metric",
+      "x": 0,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [
+            "${var.candidate_id}",
+            "account_count.value"
+          ]
+        ],
+        "period": 300,
+        "stat": "Maximum",
+        "region": "eu-west-1",
+        "title": "Responce time"
       }
     }
   ]
