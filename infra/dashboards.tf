e8d39931389a3f1c02a5cfa0ece562b11c9e4379
Habib409 <hahi003@student.kristiania.no>
2022-12-09 01:25:26 +0100

    added extra widgets v2

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -1,5 +1,6 @@
 resource "aws_cloudwatch_dashboard" "main" {
   dashboard_name = var.candidate_id
+## Jim; seriously! we can use any word here.. How cool is that?
   dashboard_body = <<DEATHSTAR
 {
   "widgets": [
@@ -19,28 +20,12 @@ resource "aws_cloudwatch_dashboard" "main" {
         "period": 300,
         "stat": "Maximum",
         "region": "eu-west-1",
-        "title": "Antall handlekurver"
-      }
-    },
-    {
-      "type": "metric",
-      "x": 0,
-      "y": 0,
-      "width": 12,
-      "height": 6,
-      "properties": {
-        "metrics": [
-          [
-            "${var.candidate_id}",
-            "account_count.value"
-          ]
-        ],
-        "period": 300,
-        "stat": "Maximum",
-        "region": "eu-west-1",
         "title": "Total sum med penger i handlekurver"
-      },
-      {
+      }
+    }
+  ],
+  "widgets": [
+    {
       "type": "metric",
       "x": 0,
       "y": 0,
@@ -58,7 +43,7 @@ resource "aws_cloudwatch_dashboard" "main" {
         "region": "eu-west-1",
         "title": "antall handlevogner"
       }
-
+    }
   ]
 }
 DEATHSTAR
